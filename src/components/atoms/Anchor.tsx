dd17930c6f14b4eedda74920961a93f1361d7f39
Zelchi <tininhakawaii@gmail.com>
2026-01-24 19:59:19 -0300

    feat: add Anchor component with styled hover effect

---
+++ src/components/atoms/Anchor.tsx
@@ -0,0 +1,36 @@
+import type { Component, ParentProps } from 'solid-js';
+import { styled } from 'solid-styled-components';
+
+const Container = styled('a')`
+    color: var(--color6);
+    position: relative;
+    text-decoration: none;
+    display: inline-block;
+    padding-bottom: 4px;
+
+    &::before {
+        content: '';
+        position: absolute;
+        width: 100%;
+        height: 4px;
+        border-radius: 4px;
+        background-color: var(--color3);
+        bottom: 0;
+        left: 0;
+        transform-origin: right;
+        transform: scaleX(0);
+        transition: transform .3s ease-in-out;
+    }
+
+    &:hover::before {
+        transform-origin: left;
+        transform: scaleX(1);
+    }
+`;
+
+const Anchor: Component<ParentProps> = (props) => {
+    const { children, ...rest } = props;
+    return <Container {...rest}>{children}</Container>;
+};
+
+export default Anchor;
